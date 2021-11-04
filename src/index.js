ecb2d2ff3e0f460aa631a45d47d760e510f1629d
Jose Alberto Soto <asotorev@gmail.com>
2021-11-04 11:50:03 -0600

    Initial commit

--- src/index.js
+++ src/index.js
@@ -3,13 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux'
+import counterReducer from './reducer';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const store = createStore(counterReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+function increment() {
+    store.dispatch({ type: "INCREMENT" })
+}
+
+function decrement() {
+    store.dispatch({ type: "DECREMENT" })
+}
+
+const Counter = () => {
+    return (
+        <div>
+            <h1>{store.getState()}</h1>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+
+        </div>
+    )
+}
+
+const render = () => ReactDOM.render(<Counter />,
+  document.getElementById('root'))
+render()
+
+store.subscribe(render);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
